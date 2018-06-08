179417a73b5da5190667df30ad42e82d0c785fb6
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-06-08 12:48:26 +0100

    save function now working

--- db/console.rb
+++ db/console.rb
@@ -7,42 +7,55 @@ customer1 = Customer.new({
   'name' => 'Dan',
   'wallet' => 1000
   })
+customer1.save()
 
 customer2 = Customer.new({
   'name' => 'Dave',
   'wallet' => 1500
   })
+customer2.save()
 
 customer3 = Customer.new({
   'name' => 'Gabby',
   'wallet' => 750
   })
+customer3.save()
 
 film1 = Film.new({
  'title' => 'Deadpool',
  'price' => 100
   })
+film1.save()
 
 film2 = Film.new({
   'title' => 'Solo',
   'price' => 50
   })
+film2.save()
 
 ticket1 = Ticket.new({
   'customer_id' => customer1.id,
   'film_id' => film1.id
   })
+  ticket1.save()
+
 ticket2 = Ticket.new ({
   'customer_id' => customer2.id,
   'film_id' => film1.id
   })
+ticket2.save()
+
 ticket3 = Ticket.new ({
   'customer_id' => customer1.id,
   'film_id' => film2.id
   })
+  ticket3.save()
+
 ticket4 = Ticket.new ({
   'customer_id' => customer3.id,
   'film_id' => film2.id
   })
+  ticket4.save()
+
   binding.pry
   nil
